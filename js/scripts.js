d2d4fd248937580c6f9e5ac8d4710e58e038cb56
shimran george & alex beeken <student@epicodus.com>
2015-02-18 15:57:47 -0800

    Add client list

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,6 @@ var BankAccount = {
     var newBalance = this.balance - amount;
     if(validateAmount(amount))
     {
-
-
       if(newBalance<0){
         if (confirm("WARNING: Your balance will be in the negative and charged a $25 overdraft fee!"))
         {
@@ -18,7 +16,7 @@ var BankAccount = {
       }
       this.balance = newBalance;
       return newBalance;
-  }
+    }
   },
   deposit: function(amount){
     var newBalance = this.balance + amount;
@@ -42,27 +40,33 @@ var validateAmount = function(amount) {
 
 
 
-var account = Object.create(BankAccount);
 
 $(document).ready(function(){
+var account = Object.create(BankAccount);
   $('form#accountCreateForm').submit(function(event){
     event.preventDefault()
 
     var name= $('input#name').val();
+    $('input#name').val("")
     var balance = parseFloat($('input#initialDeposit').val());
+    $('input#initialDeposit').val("")
 
     account.name = name;
     account.balance = balance;
 
+    $('#accountName').text(account.name);
     $('#accountBalance').text(account.balance);
+    $('#list').append("<li id='client'>" + account.name + "</li>");
   });
 
   $('form#accountTransactionForm').submit(function(event){
     event.preventDefault()
-    $('#accountBalance').click("");
 
     var deposit = parseFloat($('input#deposit').val());
     var withdrawal = parseFloat($('input#withdraw').val());
+    $('input#deposit').val("");
+    $('input#withdraw').val("");
+
 
     if (isNaN(deposit)){
       deposit=0.00;
@@ -73,8 +77,13 @@ $(document).ready(function(){
     account.deposit(parseFloat(deposit));
     account.withdraw(parseFloat(withdrawal));
 
-
     $('#accountBalance').text(account.balance.toFixed(2));
+  });
+
+  $('#client').last().click(function(event) {
+    event.preventDefault();
+    $('#accountName').text(account.name);
+    $("#acccountBalance").text(account.balance);
   });
 
 });
